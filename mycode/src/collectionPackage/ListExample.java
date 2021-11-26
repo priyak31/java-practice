fb6eebb1ba1746412e721025410150e95d2a4b49
priyak31 <63193469+priyak31@users.noreply.github.com>
2021-11-26 11:54:31 +0530

    Add files via upload

--- mycode/src/collectionPackage/ListExample.java
+++ mycode/src/collectionPackage/ListExample.java
@@ -5,23 +5,12 @@ import java.util.List;
 
 public class ListExample {
 	
-	public static int arraData[]= {0,1,3,6,7,6};
+	public static int arraData[]= {0,4,3,6,7,6};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		List<Integer> mylist=new ArrayList<Integer>();
+		List<Integer>mylist=new ArrayList<Integer>();
 		for(Integer x : arraData) {
 			System.out.println("Given array "+ x);
 		}		
 	}
 }
-
-//output
-//Given array 3
-//Given array 6
-//Given array 7
-//Given array 6
-
-//list contain duplicate values
-//maintain insertion order
-//random accsess
-//If you want to access from list, you need to write list.get(0);  
--- mycode/src/collectionPackage/QueueExample.java
+++ mycode/src/collectionPackage/QueueExample.java
@@ -1,17 +1,18 @@
 package collectionPackage;
 
-import java.util.LinkedList;
+//import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class QueueExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		   Queue<Integer> q1 = new LinkedList<Integer>();
+		   Queue<Integer> q1 = new PriorityQueue<Integer>();
 		        //Add elements to the Queue
 		        q1.add(10);
 		        q1.add(20);
-		        q1.add(30);
+		        q1.add(20);
 		        q1.add(40);
 		        q1.add(50);
 		        System.out.println("Elements in Queue:"+q1);
@@ -30,13 +31,4 @@ public class QueueExample {
 
 	
 
-//output
-//Head of the queue: 20
-//Poll():Returned Head of the queue: 20
-//peek():Head of the queue: 30
-//Final Queue:[30, 40, 50]
-//Not maintain insertion order
-//contain duplicate values
-//queue cannot contain null values although some implementations, such as LinkedList, do not prohibit insertion of null.
-// In case of PriorityQueue it gives null pointer exception.
 
