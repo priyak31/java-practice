eb547e0041d504134303969c06f0381a201ff841
priyak31 <63193469+priyak31@users.noreply.github.com>
2021-11-26 09:54:23 +0530

    Update genericsExample.java

--- mycode/src/generics/genericsExample.java
+++ mycode/src/generics/genericsExample.java
@@ -26,3 +26,14 @@ public class genericsExample {
 	}
 
 }
+
+//output
+
+//Array integerArray contains:
+//|1| |2| |3| |4| |5| 
+
+//Array doubleArray contains:
+//|1.1| |2.2| |3.3| |4.4| 
+
+//Array characterArray contains:
+//|H| |E| |L| |L| |O| 
