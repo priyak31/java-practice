4f4f42cc638370bc6e44002b85b659082d9d2d5a
priyak31 <63193469+priyak31@users.noreply.github.com>
2021-11-26 09:52:44 +0530

    Update genericExampleClass.java

--- mycode/src/generics/genericExampleClass.java
+++ mycode/src/generics/genericExampleClass.java
@@ -27,13 +27,4 @@ public class genericExampleClass<T> {
 
 
 //output
-
-//Array integerArray contains:
-//|1| |2| |3| |4| |5| 
-
-//Array doubleArray contains:
-//|1.1| |2.2| |3.3| |4.4| 
-
-//Array characterArray contains:
-//|H| |E| |L| |L| |O| 
-
+//Integer 10 String hello world
