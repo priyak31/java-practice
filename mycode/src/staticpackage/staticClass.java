ffee7798c10895eaee45387f76657a37306b25c4
priyak31 <63193469+priyak31@users.noreply.github.com>
2021-11-26 10:45:09 +0530

    Update staticClass.java

--- mycode/src/staticpackage/staticClass.java
+++ mycode/src/staticpackage/staticClass.java
@@ -28,5 +28,7 @@ public class staticClass {
 // Static mainly used for memory management means it is constant variable or method that is same for every instance of class.
 // it is not access by instance of class.
 //It saves memory.
-e.g college name,company name for employee etc
+//e.g college name,company name for employee etc
+//static method used for accessing static variable.
+//static method can't access non-static variable inside it.
 
